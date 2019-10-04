55e3597dd9faf2dd1ba38b0f45c5a80bac3511aa
Erik Heggelund <erhe@nmbu.no>
2019-10-04 12:13:55 +0200

    Wrote all the tests

---
+++ src/YOUR_NAME_ex/ex03_project/test_median.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'Erik Heggelund'
+__email__ = 'erhe@nmbu.no'
+
+import pytest
+
+
+def median(data):
+    """ Returns median of data
+    """
+    pass
--- src/YOUR_NAME_ex/ex03_project/test_sorting.py
+++ src/YOUR_NAME_ex/ex03_project/test_sorting.py
@@ -18,7 +18,7 @@ def bubble_sort(list_to_be_sorted):
                 )
             else:
                 pass
-    return tuple(sorted_list)
+    return sorted_list
 
 
 def test_empty():
@@ -30,7 +30,7 @@ def test_empty():
 def test_single():
     """Test that the sorting function works for single-element list"""
     assert len(bubble_sort([1])) == 1
-    assert bubble_sort([1]) == 1
+    assert bubble_sort([1]) == [1]
 
 
 def test_sorted_is_not_original():
@@ -45,7 +45,9 @@ def test_sorted_is_not_original():
     Now sorted_data shall be a different object than data,
     not just another name for the same object.
     """
-    pass
+    data = [3, 2, 1]
+    sorted_data = bubble_sort(data)
+    assert id(data) is not id(sorted_data)
 
 
 def test_original_unchanged():
@@ -59,22 +61,33 @@ def test_original_unchanged():
 
     Now data shall still contain [3, 2, 1].
     """
-    pass
+    data = [3, 5, 1, 24, 5, 5.0]
+    bubble_sort(data)
+    assert data == [3, 5, 1, 24, 5, 5.0]
 
 
 def test_sort_sorted():
     """Test that sorting works on sorted data."""
-    pass
+    data = [1, 6, 3, 3213, 354]
+    sorted_data = list(data)
+    sorted_data.sort()
+    assert bubble_sort(data) == sorted_data
 
 
 def test_sort_reversed():
     """Test that sorting works on reverse-sorted data."""
-    pass
+    data = [40, 30, 20, 10]
+    sorted_data = list(data)
+    sorted_data.sort()
+    assert bubble_sort(data) == sorted_data
 
 
 def test_sort_all_equal():
     """Test that sorting handles data with identical elements."""
-    pass
+    data = [0.65, 0.65, 0.65, 0.65, 0.65, 0.65]
+    assert bubble_sort(data) == data
+    assert len(bubble_sort(data)) == len(data)
+    assert bubble_sort(data)[1] == 0.65
 
 
 def test_sorting():
@@ -85,4 +98,14 @@ def test_sorting():
     ensure that they are sorted correctly. These could be lists of
     numbers of different length or lists of strings.
     """
-    pass
+    list_of_ints = [1, 6, 23125, 3, 2, 65]
+    list_of_floats = [3.6, 45452.76, 0.0000013, 31.0]
+    mixed_list_of_ints_and_floats = [1, 6, 23125, 3, 3.6, 45452.76,  31.0]
+    list_of_strings = ['A', '!', 'hei', 'Fisk er egentlig ikke dyr']
+    list_of_lists = [list_of_ints, list_of_floats,
+                     mixed_list_of_ints_and_floats, list_of_strings]
+
+    for element in list_of_lists:
+        sorted_data = list(element)
+        sorted_data.sort()
+        assert bubble_sort(element) == sorted_data
